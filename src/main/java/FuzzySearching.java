d5743d5c1386fcd977a9488f3c9e35c5cdcf9d9a
Core310 <32469530+Core310@users.noreply.github.com>
2021-12-28 23:44:49 -0600

    a

---
+++ src/main/java/FuzzySearching.java
@@ -0,0 +1,30 @@
+import me.xdrop.fuzzywuzzy.FuzzySearch;
+import me.xdrop.fuzzywuzzy.model.ExtractedResult;
+
+import java.util.ArrayList;
+
+public class FuzzySearching {
+
+    public static void main(String[] args) {
+        String baseWikiUrl = "https://ffxiv.consolegameswiki.com/wiki/";
+        int baseWikiUrlLength = baseWikiUrl.length();
+
+        String searchKey = "Lava_Toad";
+        String RealString = baseWikiUrl+searchKey;
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("https://ffxiv.consolegameswiki.com/wiki/lavatoad");
+        arrayList.add("https://ffxiv.consolegameswiki.com/wiki/toadLava");
+        arrayList.add("https://ffxiv.consolegameswiki.com/wiki/");
+        arrayList.add("https://ffxiv.consolegameswiki.com/wiki/lavaegg");
+        arrayList.add(RealString);
+        if (RealString.length() == baseWikiUrlLength)
+            System.out.println("No value found to inset, todo pre-defined error ");
+        //above is just some base case that wuld prevent from looping thru whole list
+        //then we loop thru the list
+
+        ExtractedResult ratio = FuzzySearch.extractOne("lava toad",
+                arrayList);
+        System.out.println(ratio);
+    }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,37 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("Thingy to find: ");
+        String part = "";
+        String wikiPage = "https://ffxiv.consolegameswiki.com/wiki/Botanist_Node_Locations";
+        String gatheringPage = "https://na.finalfantasyxiv.com/lodestone/playguide/db/gathering/";
+        Document doc = Jsoup.connect(wikiPage + part).get();//Possible to get a webpage that is super close 2 it?
+        // like auto google smtn
+
+        boolean firstSkipped = false;
+
+        ArrayList<Elements> tds = new ArrayList<Elements>();
+
+        for(Element element : doc.select("tr") ) {
+            // Skip the first 'tr' tag since it's the header
+            if (!firstSkipped) {
+                firstSkipped = true;
+                continue;
+            }
+
+            Elements td = element.select("td");
+            tds.add(td);
+        }
+
+        System.out.println(tds);
+    }
+}
