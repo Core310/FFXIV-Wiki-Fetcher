59037c1d2cd678f31c853c89b52fb75e28824c69
Something Something <arika.khor@gmail.com>
2023-08-12 15:06:15 +0800

    Api refactor (#66)
    * added jackson to maven
    
    * made formatGrouped zones public. Tidy some other stuff

--- src/main/java/ffxivWikiFinder/Main.java
+++ src/main/java/ffxivWikiFinder/Main.java
@@ -21,6 +21,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         ListFinder.addItem(new String[]{"clod jelfish", "crim trou", "rarefield reef roc", "yugar salmon", "giantpiel"});
+        //This should build off the current libary and make into a simple API. So the only thing that changes here is making formatGroupedZones public. Then make another part of this
+        // We query listFinder input from API string stream. Then we return the ListFinder formatGroupedZones as a json object. The json object is then given to the end node to be delt with
+        // however. The input could be in a table form, then just press tab to go next.
         System.out.println(ListFinder.outPut());
     }
 
